f6d33262c13584d618b5f2dd4c206fa4b0ffd2dd
Jeremy Long <jlong@eventbrite.com>
2018-11-18 18:00:49 -0600

    updating the section 3 app.py

--- Py_APIs/app.py
+++ Py_APIs/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ stores = [
 		'name': 'My Wonderful Store',
 		'items': [
 			{
-			'name': 'My Item'
+			'name': 'My Item',
 			'price': 15.99
 			}
 		]
@@ -18,20 +19,20 @@ stores = [
 # GET - used to send data back only
 
 # POST /store data: {name:}
-@app.route('/store', methods=['POST'])  
+@app.route('/store', methods=['POST'])
 def create_store():
 	pass
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>')
-def get_store(name)
+def get_store(name):
 	pass
 
 
 # GET /store
 @app.route('/store')
-def get_store():
-	pass
+def get_stores():
+	return jsonify({'stores': stores})
 
 # POST /store/<string:name>/item {name:, price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
